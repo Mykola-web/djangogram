3bf164616cacaefa0b909360f2c4ef0b56c91f1a
Mykola Karandiei <korondei@gmail.com>
2025-04-13 16:40:13 +0300

    corrected email link  for current deploy

--- insta/myapp/services.py
+++ insta/myapp/services.py
@@ -16,7 +16,7 @@ def generate_activation_link(user):
     if os.getenv('ENVIRONMENT') == 'development':
         activation_link = f"http://127.0.0.1:8000/activate/{uid}/{token}/"
     else:
-        activation_link = f"https://djangoogram-b428371d1362.herokuapp.com/activate/{uid}/{token}"
+        activation_link = f"https://djangogramm1-1620dd96cc61.herokuapp.com/activate/{uid}/{token}"
     return activation_link
 
 
