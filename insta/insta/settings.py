eb3ecafaea9e2deb1719050c8038e016bbcd884d
Mykola Karandiei <korondei@gmail.com>
2025-05-22 22:05:50 +0300

    env changes2

--- insta/insta/settings.py
+++ insta/insta/settings.py
@@ -102,6 +102,9 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
     'default': dj_database_url.config(default = DATABASE_URL)
 }
+print("ENVIRONMENT =", ENVIRONMENT)
+print("DATABASE_URL =", DATABASE_URL)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
