9ac873447d03a52fe1de44f3ab9f573c03ee194a
Mykola Karandiei <korondei@gmail.com>
2025-08-12 22:52:13 +0300

    deleted signal for deleting local files

--- insta/myapp/signals.py
+++ insta/myapp/signals.py
@@ -10,14 +10,3 @@ from .models import ProfileModel, PostModel
 def create_profile(sender, instance, created, **kwargs):
     if created:
         ProfileModel.objects.create(user = instance)
-
-
-@receiver(pre_delete, sender = PostModel)
-def delete_image(sender, instance, **kwargs):
-    if instance.image:
-        image_path = instance.image.path
-        if os.path.isfile(image_path):
-            print(f"Deleting image at {image_path}")
-            os.remove(image_path)
-        else:
-            print(f"Image not found at {image_path}")
